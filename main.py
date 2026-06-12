85739d309609fd797feaa2243ba680870e8d6ae8
Franklin <75178119@continental.edu.pe>
2026-06-12 12:53:08 -0500

    Segundo commit después de clonar

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("Quien ganara keiko o jp")
 print("Ya casi termina el semestre wiii")
+
+y = int(input("Ingrese un numero:"))
+x = int(input("Ingrese un numero: "))
+
+z = y + x
+print(f"El resultado es {z}")
